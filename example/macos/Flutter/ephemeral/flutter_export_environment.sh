d8250c14ea6cabcceba27c72a4c01cacefb5b863
Aghiad Odeh <aghiadodeh@gmail.com>
2024-09-05 16:52:57 +0300

    Create flutter_export_environment.sh

---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aghiad/Documents/Programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aghiad/Projects/Github/animated_infinite_scroll_pagination/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
